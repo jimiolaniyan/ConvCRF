94110aaa00a467803b55db769a7525d93fbc72b9
root <root@crf-semantic-segmentation-vm.europe-west1-b.c.crfsemseg.internal>
2018-11-15 13:31:42 +0000

    update training, add resnetfcn skeleton

---
+++ models/resnet_fcn.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class ResNetFCN(nn.Module):
+    def __init__(self):
+        super(ResNetFCN, self).__init__()
+        
+    def forward(self):
+        pass
+        
